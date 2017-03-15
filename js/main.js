29c9ff3e1ca9afe9be71218b41422e61493b7235
carlosscc <carlosscc>
2017-03-15 13:36:37 +0100

    Ejercicios JavaScript y algunas modificaciones

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $('#mycarousel').carousel({
-		interval: 10000
-	});
+	interval: 15000
+});
+
+var spreadsheetID = '1VhzZaWAi_BExbA6j7NZLP3cZquRO4-IQ4-S3e0ROtzI',
+spreadsheetTAB = 1,
+spreadsheetURL = 'https://spreadsheets.google.com/feeds/list/' + spreadsheetID + '/' + spreadsheetTAB + '/public/values';
+
+var datos;
+// Pedimos un recurso por AJAX usando JSON
+$.getJSON( spreadsheetURL + '?alt=json', function( data ) {
+	console.log(data);
+});
+
+var p = document.getElementById('copyright');
+	p.innerHTML = "&copy; 2017 Carlos Sánchez-Castro Coy";
+	p.style = 'color: #0275d8';
+  console.log('p = ', p.getAttribute('style'), p.innerHTML);
 
 
 /*var shiftWindow = function() { scrollBy(0, -70); };
